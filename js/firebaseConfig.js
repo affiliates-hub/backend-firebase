3f40ab48706a3895c8041bf8a5346fefdcd1deee
affiliates-hub <affiliates665@gmail.com>
2024-08-04 18:54:26 +0530

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,9 +1,10 @@
 var config = {
-    apiKey: "AIzaSyCiFb0LRM8yFDKBfN0ZEqPRmEadr7I4Jlo",
-    authDomain: "playorbit-fbfaf.firebaseapp.com",
-    databaseURL: "https://playorbit-fbfaf.firebaseio.com",
-    projectId: "playorbit-fbfaf",
-    storageBucket: "playorbit-fbfaf.appspot.com",
-    messagingSenderId: "107154671391"
+    apiKey: "AIzaSyBlbZqYgmRWnfFXrL0Kq8Gq7KL3IyTKN10",
+    authDomain: "affiliates-hub.firebaseapp.com",
+    projectId: "affiliates-hub",
+    storageBucket: "affiliates-hub.appspot.com",
+    messagingSenderId: "324031673574",
+    appId: "1:324031673574:web:9e8ada977e2c1d9ef53727",
+  measurementId: "G-HC60ZJVKRG"
   };
   firebase.initializeApp(config);
